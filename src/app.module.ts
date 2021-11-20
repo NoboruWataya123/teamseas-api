4548ed67294b38476574f390f4155a0120311585
P00po <dulustan1996@gmail.com>
2021-11-20 14:32:24 +0900

    feat: install graphql initial configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 
 @Module({
-  imports: [],
+  imports: [PostModule, UserModule, AuthenticationModule, GraphQLModule.forRoot({
+    typePaths: ['./**/*.graphql'],
+    playground: false,
+    plugins: [ ApolloServerPluginLandingPageLocalDefault() ],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
